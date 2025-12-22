9f8ccc002c5895af1b3b3720fb0f09a74ca78ac5
Abdallah-Ehab <abdallahehab3710@gmail.com>
2025-12-22 18:37:36 +0200

    refactored form validation code

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-
-
-
-
 const userNameInput = document.querySelector(".userName")
 const passwordInput = document.querySelector(".password")
 const emailInput = document.querySelector(".email")
@@ -9,53 +5,44 @@ const emailInput = document.querySelector(".email")
 
 const errorElements = document.querySelectorAll(".error")
 
-let inputs = []
-
-console.log(errorElements)
-userNameInput.addEventListener("input",(e)=>{
-    let usernameError = errorElements[0]
-    if(!validateUserName(e.target.value) && e.target.value != "" ){
-        let message = document.createElement("p")
-        message.textContent = "user-name should be 2-20 characters long, no special characters or spaces"
-        usernameError.append(message)
-    }else{
-        const p = usernameError.querySelector("p")
-        if(p){
-
-            usernameError.removeChild("p")
-        }
+let inputsAndValidators = [
+    {
+        input: userNameInput,
+        validator: validateUserName,
+        error: "user-name should be 2-20 characters long, no special characters or spaces"
+        ,errorElement : errorElements[0]
+    },{
+        input: passwordInput,
+        validator: validatePassword,
+        error: "password should be atleast 8 characters, with capital,small and special characters",
+        errorElement : errorElements[2]
+    },{
+        input: emailInput,
+        validator: validateEmail,
+        error: "enter a valid email"
+        ,errorElement: errorElements[2]
     }
-})
+]
 
-emailInput.addEventListener("input",(e)=>{
-    let emailError = errorElements[1]
-    if(!validateEmail(e.target.value) && e.target.value != "" ){
-        let message = document.createElement("p")
-        message.textContent = "enter a valid email"
-        emailError.append(message)
-    }else{
-        const p = emailError.querySelector("p")
-        if(p){
-
-            emailError.removeChild("p")
+inputsAndValidators.forEach((form,index)=>{
+    form.input.addEventListener("input",(e)=>{
+        let errorElement = form.errorElement
+        if(!form.validator(e.target.value) && e.target.value != "" ){
+            let message = document.createElement("p")
+            message.textContent = form.error
+            if(!errorElement.firstChild)
+                {
+                
+                    errorElement.append(message)
+                }
+                    
+        }else{
+            const p = errorElement.querySelector("p")
+            if(p){
+                errorElement.removeChild(p)
+            }
         }
-    }
-})
-
-passwordInput.addEventListener("input",(e)=>{
-    let passwordError = errorElements[2]
-    if(!validateUserName(e.target.value) && e.target.value != "" ){
-        let message = document.createElement("p")
-        message.textContent = "password should be atleast 8 characters, with capital,small and special characters"
-        passwordError.append(message)
-    }else{
-        const p = passwordError.querySelector("p")
-        if(p){
-        
-            passwordError.removeChild("p")
-        }
-            
-    }
+    })
 })
 
 
